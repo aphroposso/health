c10a9b7d89fb94f27537ccd7459ce6663844ae38
aphroposso <hakuzweaphrodis@gmail.com>
2023-10-23 19:38:52 +0200

    update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,32 +1,27 @@
- // listen to the form submission
- document
- .getElementById("form-group")
- .addEventListener("submit", function (event) {
-   event.preventDefault();
 
-   // send the email here
-  emailjs.sendForm('service_ky7cpsc', 'template_1mom4sb', '#contact-form').then(
-     (response) => {
-       console.log("SUCCESS!", response.status, response.text);
-       alert("YOUR MESSAGE HAVE BEEN SENT SUCCESSFULLY!");
-       setContactForm(""); // Resetting the contact-form input field after successful submission
-       setLoading(false);
+ document.getElementById("contact-form").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-setTimeout(() => {
-  setErrorMessage("");
-}, 3000);
-     },
-     (error) => {
-       console.log("FAILED...", error);
-       alert("Oops it is not you fault..", error);
-     }
-   );
-   setContactForm(""); // Resetting the contact-form input field after successful submission
-   setLoading(false);
+  emailjs.sendForm('service_ky7cpsc', 'template_1mom4sb', '#contact-form', this).then(
+    function (response) {
+      // Form submission was successful
+      console.log("Email sent successfully");
+      document.getElementById("email").style.display = "none";
+      document.getElementsByName("message")[0].style.display = "none";
 
-setTimeout(() => {
-  setErrorMessage("");
-}, 3000);
- });
+      // Display response status
+      const responseDiv = document.createElement("div");
+      responseDiv.textContent = "Response status: " + response.status;
+      document.body.appendChild(responseDiv);
 
- 
+      setTimeout(function () {
+        // Hide the response status after 2 seconds
+        responseDiv.style.display = "none";
+      }, 2000);
+    },
+    function (error) {
+      // Form submission failed
+      console.error("Email sending failed: " + JSON.stringify(error));
+    }
+  );
+});
