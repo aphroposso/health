adf5b43437be3062441d7f7ab6394db9fb99d237
aphroposso <hakuzweaphrodis@gmail.com>
2023-10-23 15:53:25 +0200

    update index.html, and adding contact.js

---
+++ js/contact.js
@@ -0,0 +1,32 @@
+ // listen to the form submission
+ document
+ .getElementById("contact-form")
+ .addEventListener("submit", function (event) {
+   event.preventDefault();
+
+   // send the email here
+  emailjs.sendForm('service_ky7cpsc', 'template_1mom4sb', '#contact-form').then(
+     (response) => {
+       console.log("SUCCESS!", response.status, response.text);
+       alert("YOUR MESSAGE HAVE BEEN SENT SUCCESSFULLY!");
+       setContactForm(""); // Resetting the contact-form input field after successful submission
+       setLoading(false);
+
+setTimeout(() => {
+  setErrorMessage("");
+}, 3000);
+     },
+     (error) => {
+       console.log("FAILED...", error);
+       alert("Oops it is not you fault..", error);
+     }
+   );
+   setContactForm(""); // Resetting the contact-form input field after successful submission
+   setLoading(false);
+
+setTimeout(() => {
+  setErrorMessage("");
+}, 3000);
+ });
+
+ 
