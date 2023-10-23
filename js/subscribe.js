53a24567e7d9e051d069e1a98eb47fe159fbc916
aphroposso <hakuzweaphrodis@gmail.com>
2023-10-23 19:55:33 +0200

    update subscribe.js

--- js/subscribe.js
+++ js/subscribe.js
@@ -1,29 +1,34 @@
 document.getElementById('convertkit-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the default form submission
-  
+
     const emailInput = document.querySelector('.common-input');
     const formId = '5693494'; // Replace with your actual ConvertKit form ID
     const apiKey = 'P0oWAiAmJiX_vkmZRgK8aw'; // Replace with your ConvertKit API key
-  
+
     // Remove capitalized Gmail and .com
     let email = emailInput.value.toLowerCase(); // Convert to lowercase
     email = email.replace('gmail', ''); // Remove 'gmail'
     email = email.replace('.com', ''); // Remove '.com'
-  
+
     const data = {
         email: email,
-        api_key: apiKey,
         tags: [], // You can add tags if needed
     };
-  
-    fetch(`https://app.convertkit.com/forms/designers/5693494/newsletter-inner`, {
+
+    fetch(`https://api.convertkit.com/v3/forms/${formId}/subscribe`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`,
         },
         body: JSON.stringify(data),
     })
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
     .then(result => {
         if (result.subscription.state === 'active') {
             document.getElementById('success-message').textContent = 'Subscription successful!';
@@ -42,6 +47,7 @@ document.getElementById('convertkit-form').addEventListener('submit', function(e
         }
     })
     .catch(error => {
+        console.error('Error:', error);
         document.getElementById('error-message').textContent = 'An error occurred. Please try again later.';
         document.getElementById('success-message').textContent = '';
         setTimeout(function() {
@@ -49,4 +55,4 @@ document.getElementById('convertkit-form').addEventListener('submit', function(e
             emailInput.value = ''; // Clear the email input field
         }, 5000); // Clear error message and email input after 5 seconds
     });
-  });
+});
