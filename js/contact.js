93affa47193ea0fe24843378624d559883d72d09
aphroposso <hakuzweaphrodis@gmail.com>
2023-11-07 17:03:16 +0200

    update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,42 +1,22 @@
 
-document.getElementById("contact-form").addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const phone = document.getElementById("phone").value;
-  const occupation = document.getElementById("occupation").value; // Change 'subject' to 'occupation'
-  const message = document.getElementById("message").value;
-
-  // Check if the phone number starts with "078" and has a total length of 10
-  if (phone.startsWith("078") && phone.length === 10) {
-    emailjs.send('service_ky7cpsc', 'template_ndnn34j', {
-      name: name,
-      email: email,
-      phone: phone,
-      occupation: occupation,
-      message: message
-  })
-  .then((response) => {
-      console.log("Email sent successfully:", response);
-      document.getElementById("success-message").textContent = "Your message has been sent successfully!";
-      clearMessagesAndFormAfterDelay();
-  })
-  .catch((error) => {
-      console.error("Email sending failed:", error);
-      document.getElementById("error-message").textContent = "Oops, there was an error while sending the email. Please try again later.";
-  });
-  } else {
-      document.getElementById("error-message").textContent = "Phone number must start with '078' and have a total length of 10.";
-  }
+// the form id is myForm
+$('#contact-form').on('submit', function(event) {
+    event.preventDefault(); // prevent reload
+    
+    var formData = new FormData(this);
+    formData.append('service_id', 'service_ky7cpsc');
+    formData.append('template_id', 'template_ndnn34j');
+    formData.append('user_id', 'VxesBWGzF0LgYlc9N');
+ 
+    $.ajax('https://api.emailjs.com/api/v1.0/email/send-form', {
+        type: 'POST',
+        data: formData,
+        contentType: false, // auto-detection
+        processData: false // no need to parse formData to string
+    }).done(function() {
+        alert('Your mail is sent!');
+    }).fail(function(error) {
+        alert('Oops... ' + JSON.stringify(error));
+    });
 });
-
-function clearMessagesAndForm() {
-  document.getElementById("error-message").innerHTML = "";
-  document.getElementById("success-message").innerHTML = "";
-  document.getElementById("contact-form").reset();
-}
-
-function clearMessagesAndFormAfterDelay() {
-  setTimeout(clearMessagesAndForm, 1000);
-}
+// code fragment
