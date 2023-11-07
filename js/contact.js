15af23b734880d1f8fbf886ae8e1478ad32cc90e
aphroposso <hakuzweaphrodis@gmail.com>
2023-11-07 15:12:03 +0200

    update contact.js

--- js/contact.js
+++ js/contact.js
@@ -10,22 +10,22 @@ document.getElementById("contact-form").addEventListener("submit", function (eve
 
   // Check if the phone number starts with "078" and has a total length of 10
   if (phone.startsWith("078") && phone.length === 10) {
-      emailjs.send('service_ky7cpsc', 'template_ndnn34j', { 
-        name: name,
-        email: email,
-        phone: phone,
-        occupation: occupation,
-        message: message
-      })
-      .then((response) => {
-          console.log("Email sent successfully:", response);
-          document.getElementById("success-message").textContent = "Your message has been sent successfully!";
-          clearMessagesAndFormAfterDelay();
-      })
-      .catch((error) => {
-          console.error("Email sending failed:", error);
-          document.getElementById("error-message").textContent = "Oops, there was an error while sending the email. Please try again later.";
-      });
+    emailjs.send('service_ky7cpsc', 'template_ndnn34j', {
+      name: name,
+      email: email,
+      phone: phone,
+      occupation: occupation,
+      message: message
+  })
+  .then((response) => {
+      console.log("Email sent successfully:", response);
+      document.getElementById("success-message").textContent = "Your message has been sent successfully!";
+      clearMessagesAndFormAfterDelay();
+  })
+  .catch((error) => {
+      console.error("Email sending failed:", error);
+      document.getElementById("error-message").textContent = "Oops, there was an error while sending the email. Please try again later.";
+  });
   } else {
       document.getElementById("error-message").textContent = "Phone number must start with '078' and have a total length of 10.";
   }
