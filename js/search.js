e027431931b8b2fdc9934f9a98e7658f3d7c835e
aphroposso <hakuzweaphrodis@gmail.com>
2023-11-09 10:10:30 +0200

    update

---
+++ js/search.js
@@ -0,0 +1,9 @@
+const body = document.querySelector("main-sidebar"),
+      searchToggle = document.querySelector(".search");
+
+// js code to toggle search box
+        search.addEventListener("click" , () =>{
+        search.classList.toggle("active");
+      });
+
+
